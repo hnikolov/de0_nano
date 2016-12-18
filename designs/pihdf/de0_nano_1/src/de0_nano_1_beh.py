8425e12c1eb9f354a3b611b4ebf2ad87aee62e52
hristo <h.n.nikolov@gmail.com>
2016-12-18 23:43:19 +0100

    Added a pihdf design to access the 2 buttons, 4 dipswitches, and 8 leds on the fpga board.

---
+++ designs/pihdf/de0_nano_1/src/de0_nano_1_beh.py
@@ -0,0 +1,12 @@
+import pihdf
+
+def de0_nano_1_beh(mode_1, mode_2, LEDs, LED_rdy_en, LED_rdy_buff, LED_rdy_out):
+    '''|
+    | Specify the behavior, describe data processing; there is no notion
+    | of clock. Access the in/out interfaces via get() and append()
+    | methods. The "de0_nano_1_beh" function does not return values.
+    |________'''
+
+#    print "Warning: Behavior model not implemented yet!"
+    pihdf.warn("Behavior model not implemented yet!")
+
